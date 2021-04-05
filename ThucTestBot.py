9eb78317627778454247626d544ef1bc24471042
Thuc Nguyen <thucnguyen61098@gamil.com>
2021-04-05 11:18:07 -0400

    Added function skeletons for new bot commands roll_dice and weather

--- ThucTestBot.py
+++ ThucTestBot.py
@@ -13,19 +13,35 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix='!')
 
-@bot.command(name='yo')
+@bot.command(name='yo', help='Responds with a greeting message')
 async def yo(ctx):
     bot_messages = [
             'Hello Thuc!',
             'Hey there Thuc!',
             'Howdy partner!',
             (
-                'The weather sure is nice today'
+                'The weather sure is nice today\n'
                 'Wouldn\'t you say'
             ),
     ]
     response = random.choice(bot_messages)
     await ctx.send(response)
+
+# TODO: incorporate a weather API to pull weather data and send as response
+@bot.command(name='weather', help='Checks the weather for today')
+async def weather(ctx):
+    response = 'The weather today is'
+    await ctx.send(response)
+
+@bot.command(name='roll_dice', help='Simulates rolling dice')
+async def roll(ctx, num_dice: int, num_sides: int):
+    dice = [
+            str(random.choice(range(1, num_sides + 1)))
+            for _ in range(num_dice)
+    ]
+    await ctx.send(', '.join(dice))
+
+
 
 @bot.event
 async def on_error(event, *args, **kwargs):
